8b240d2d2909bf6826653ecdd5152e711da21f6d
EricksonDutra <ericksond10@gmail.com>
2021-09-04 17:33:13 -0400

    User entity and resource

---
+++ src/main/java/com/ericksdev/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.ericksdev.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ericksdev.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "98464994", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
