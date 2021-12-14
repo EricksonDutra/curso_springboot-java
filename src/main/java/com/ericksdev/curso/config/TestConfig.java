9be9e55895827fe05d31b95ca1536f33d0f89935
EricksonDutra <ericksond10@gmail.com>
2021-12-13 23:14:40 -0400

    Payment, one-to-one association

--- src/main/java/com/ericksdev/curso/config/TestConfig.java
+++ src/main/java/com/ericksdev/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ericksdev.curso.entities.Category;
 import com.ericksdev.curso.entities.Order;
 import com.ericksdev.curso.entities.OrderItem;
+import com.ericksdev.curso.entities.Payment;
 import com.ericksdev.curso.entities.Product;
 import com.ericksdev.curso.entities.User;
 import com.ericksdev.curso.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,  Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 }
